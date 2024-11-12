6cdb812bed78ba08b430023c3dbf394666bce6df
Shlomo G <shlomo@Shlomos-MBP.lan>
2024-11-12 03:11:13 -0600

    l

--- server.js
+++ server.js
@@ -1,74 +1,79 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const db = require('./db');
-const { models: { Nationality, Painter, Style, Painting } } = db;
+const db = require("./db");
+const {
+  models: { Nationality, Painter, Style, Painting },
+} = db;
 
-app.get('/', async(req, res, next) => {
-    res.redirect('/paintings')
-})
-
-app.get('/paintings', async(req, res, next) => {
-    try {
-        
-        const paintings = await Painting.findAll({
-            include: [ Painter, Style ]
-        });
-
-        const painters = await paintings.map(painting => Painter.findByPk(painting.painterId).name;
-
-
-        res.send(`
-                <html>
-                    <head></head>
-                    <body>
-                        <div>${paintings.map((painting, idx) => `<li>${painting.name}, by ${painters[idx]}</li>`).join('')}</div>
-                    </body>
-                </html>`
-        )
-    } 
-    catch(ex) { 
-        next(ex) 
-    }
+app.get("/", async (req, res, next) => {
+  res.redirect("/paintings");
 });
 
-app.get('/painters', async (req, res, next) => {
-    try {
-        const painters = await Painter.findAll({
-            // include: [ Nationality ]
-        });
+app.get("/paintings", async (req, res, next) => {
+  try {
+    const paintings = await Painting.findAll({
+      include: [Painter, Style],
+    });
 
-        res.send(`
+    const painters = await paintings.map(
+      (painting) => Painter.findByPk(painting.painterId).name
+    );
+
+    res.send(`
                 <html>
                     <head></head>
                     <body>
-                        <div>${painters.map(painter => `<li>${painter.name}</li>`)}</div>
+                        <div>${paintings
+                          .map(
+                            (painting, idx) =>
+                              `<li>${painting.name}, by ${painters[idx]}</li>`
+                          )
+                          .join("")}</div>
                     </body>
-                </html>`
-        )
-        console.log("LOOK HERE NOW!!! - " + painters)
-    }
-    catch (ex) {
-        next(ex)
-    }
-})
+                </html>`);
+  } catch (ex) {
+    next(ex);
+  }
+});
 
-app.get('/painters/:id', async(req, res, next) => {
-    try {
-        const painter = await Painter.findByPk(req.params.id);
-        res.send(await painter.findPaintings())
+app.get("/painters", async (req, res, next) => {
+  try {
+    const painters = await Painter.findAll({
+      // include: [ Nationality ]
+    });
 
-    }
-    catch(ex) {
-        next(ex)
-    }
-}) 
+    res.send(`
+                <html>
+                    <head></head>
+                    <body>
+                        <div>${painters.map(
+                          (painter) => `<li>${painter.name}</li>`
+                        )}</div>
+                    </body>
+                </html>`);
+    console.log("LOOK HERE NOW!!! - " + painters);
+  } catch (ex) {
+    next(ex);
+  }
+});
 
-const liftoff = async() => {
-    try {
-        await db.syncAndSeed();
-        const port = process.env.port || 1338;
-        app.listen(port, () => console.log(`listening on ${port}`))
-    } catch(ex) { console.log(ex) }
+app.get("/painters/:id", async (req, res, next) => {
+  try {
+    const painter = await Painter.findByPk(req.params.id);
+    res.send(await painter.findPaintings());
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+const liftoff = async () => {
+  try {
+    await db.syncAndSeed();
+    const port = process.env.port || 1338;
+    app.listen(port, () => console.log(`listening on ${port}`));
+  } catch (ex) {
+    console.log(ex);
+  }
 };
 
 liftoff();
